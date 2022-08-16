2aa2468c765faeac263e6af73fca6f8bc72762f5
ryskit <ryosukekida.71@gmail.com>
2022-08-17 01:44:32 +0900

    :sparkles: done 4.1.2

---
+++ src/main/scala/monads/exercise4_1/Exercise4_1.scala
@@ -0,0 +1,12 @@
+package monads.exercise4_1
+
+object Exercise4_1 extends App {
+  trait Monad[F[_]] {
+    def pure[A](a: A): F[A]
+
+    def flatMap[A, B](value: F[A])(func: A => F[B]): F[B]
+
+    def map[A, B](value: F[A])(func: A => B): F[B] =
+      flatMap(value)(a => pure(func(a)))
+  }
+}

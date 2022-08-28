a6a1b9a25856ce97ef30d2e70fe9dafe56903c39
ryskit <ryosukekida.71@gmail.com>
2022-08-28 20:00:36 +0900

    :sparkles: add 7.1.2 Exercise: Reflecting on Folds

---
+++ src/main/scala/foldable_and_traverse/exercise7_1/Exercise7_1.scala
@@ -0,0 +1,7 @@
+package foldable_and_traverse.exercise7_1
+
+object Exercise7_1 extends App {
+
+  println(List(1, 2, 3).foldLeft(List.empty[Int])((acc, i) => i :: acc))
+  println(List(1, 2, 3).foldRight(List.empty[Int])((i, acc) => i :: acc))
+}

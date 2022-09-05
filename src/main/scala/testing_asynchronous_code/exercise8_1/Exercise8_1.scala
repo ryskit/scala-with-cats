f0ff37dbc7b55ae6c122ba6c57bc76c9a7d02850
ryskit <ryosukekida.71@gmail.com>
2022-09-05 20:55:31 +0900

    :sparkles: done 8.1 ~ Exercise

--- src/main/scala/testing_asynchronous_code/exercise8_1/Exercise8_1.scala
+++ src/main/scala/testing_asynchronous_code/exercise8_1/Exercise8_1.scala
@@ -1,6 +1,8 @@
 package testing_asynchronous_code.exercise8_1
 
-import cats.Id
+import cats.implicits.toTraverseOps
+import cats.syntax.functor._
+import cats.{Applicative, Id}
 
 import scala.concurrent.Future
 
@@ -22,4 +24,31 @@ object Exercise8_1 extends App {
     override def getUptime(hostname: String): Id[Int] =
       hosts.getOrElse(hostname, 0)
   }
+
+  class UptimeService[F[_]](client: UptimeClient[F]) {
+    def getTotalUptime(hostnames: List[String]): F[Int] = ???
+  }
+
+  class UptimeService2[F[_]](client: UptimeClient[F])(implicit
+    a: Applicative[F]
+  ) {
+    def getTotalUptime(hostnames: List[String]): F[Int] =
+      hostnames.traverse(client.getUptime).map(_.sum)
+  }
+
+  class UptimeService3[F[_]: Applicative](client: UptimeClient[F]) {
+    def getTotalUptime(hostnames: List[String]): F[Int] =
+      hostnames.traverse(client.getUptime).map(_.sum)
+  }
+
+  def testTotalUptime() = {
+    val hosts    = Map("host1" -> 10, "host2" -> 6)
+    val client   = new TestUptimeClient2(hosts)
+    val service  = new UptimeService2(client)
+    val actual   = service.getTotalUptime(hosts.keys.toList)
+    val expected = hosts.values.sum
+    assert(actual == expected)
+  }
+
+  testTotalUptime()
 }

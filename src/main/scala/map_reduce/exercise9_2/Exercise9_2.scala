04145a0bee7ee5d333e0679660b6be6a3ba0e98c
ryskit <ryosukekida.71@gmail.com>
2022-09-05 21:15:38 +0900

    :sparkles: 9.2 Exercise

---
+++ src/main/scala/map_reduce/exercise9_2/Exercise9_2.scala
@@ -0,0 +1,18 @@
+package map_reduce.exercise9_2
+
+import cats.Monoid
+import cats.syntax.monoid._
+import cats.instances.int._
+import cats.instances.string._
+
+object Exercise9_2 extends App {
+
+  def foldMap[A, B: Monoid](v: Vector[A])(f: A => B): B =
+    v.map(f).foldLeft(Monoid.empty[B])(_ |+| _)
+
+  println(foldMap(Vector(1, 2, 3))(identity))
+
+  println(foldMap(Vector(1, 2, 3))(_.toString + "! "))
+
+  println(foldMap("Hello world!".toVector)(_.toString.toUpperCase))
+}
